36e1379b4f41d454823088bca52eed8385a13fd3
Diego Oliveira <diegopo@motorola.com>
2022-08-18 10:46:21 -0300

    first commit

---
+++ Aula_03/app/src/main/java/br/org/venturus/kotlinjava/java/UserKotlin.kt
@@ -0,0 +1,12 @@
+package br.org.venturus.kotlinjava.java
+
+class UserKotlin {
+
+    var firstName: String = ""
+    var lastName: String = ""
+
+    constructor(firstName: String, lastName: String) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
---
+++ TrilhaSoftex/app/src/main/java/com/example/trilhasoftex/House.kt
@@ -0,0 +1,4 @@
+package com.example.trilhasoftex
+
+class House {
+}
